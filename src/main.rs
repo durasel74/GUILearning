405acc86f6c08f894b2d0e06583cccaee67806f7
durasel74 <diman174cheb@mail.ru>
2022-10-23 21:58:54 +0500

    Panels work

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,7 @@ enum Lessons {
 
 struct MyApp {
     current_lesson: Lessons,
+    test_str: String,
 }
 
 impl MyApp {
@@ -24,20 +25,42 @@ impl MyApp {
         Self::default()
     }
 
-    fn draw_lesson1(ctx: &egui::Context, ui: &mut egui::Ui) -> Response {
-        ui.button("text")
+    fn draw_lesson1(&mut self, ctx: &egui::Context) {
+        egui::TopBottomPanel::top("main_top_panel").resizable(true).show(ctx, |ui| {
+            // egui::SidePanel::left("left_top_panel").resizable(true).show(ctx, |ui| {
+            //     ui.collapsing("CollapsingHeader", |ui| {
+            //         ui.label("Body"); 
+            //     }).header_response;
+            // });
+            
+            // egui::SidePanel::right("right_top_panel").resizable(true).show(ctx, |ui| {
+            //     ui.collapsing("CollapsingHeader2", |ui| {
+            //         ui.label("Body"); 
+            //     }).header_response;
+            // });
+            
+            ui.text_edit_multiline(&mut self.test_str);
+        });
+
+        egui::TopBottomPanel::bottom("main_bottom_panel").resizable(true).show(ctx, |ui| {
+            ui.collapsing("CollapsingHeader3", |ui| {
+                ui.label("Body"); 
+            }).header_response;
+
+            ui.text_edit_multiline(&mut self.test_str);
+        });
     }
 
-    fn draw_lesson2(ctx: &egui::Context, ui: &mut egui::Ui) -> Response {
-        ui.button("text2")
+    fn draw_lesson2(ctx: &egui::Context, ui: &mut egui::Ui) {
+        ui.button("text2");
     }
 
-    fn draw_lesson3(ctx: &egui::Context, ui: &mut egui::Ui) -> Response {
-        ui.button("text3")
+    fn draw_lesson3(ctx: &egui::Context, ui: &mut egui::Ui) {
+        ui.button("text3");
     }
 
-    fn draw_lesson4(ctx: &egui::Context, ui: &mut egui::Ui) -> Response {
-        ui.button("text4")
+    fn draw_lesson4(ctx: &egui::Context, ui: &mut egui::Ui) {
+        ui.button("text4");
     }
 }
 
@@ -45,6 +68,7 @@ impl Default for MyApp {
     fn default() -> Self {
         Self {
             current_lesson: Lessons::Less1,
+            test_str: String::new(),
         }
     }
 }
@@ -56,7 +80,7 @@ impl eframe::App for MyApp {
             .fill(egui::Color32::DARK_GRAY);
 
         egui::SidePanel::left("left_side_bar").resizable(false).frame(frame).show(ctx, |ui| {
-            ui.selectable_value(&mut self.current_lesson, Lessons::Less1, "Первый урок");
+            ui.selectable_value(&mut self.current_lesson, Lessons::Less1, "Контейнеры и компоновка");
             ui.selectable_value(&mut self.current_lesson, Lessons::Less2, "Второй урок");
             ui.selectable_value(&mut self.current_lesson, Lessons::Less3, "Третий урок");
             ui.selectable_value(&mut self.current_lesson, Lessons::Less4, "Четвертый урок");
@@ -64,7 +88,7 @@ impl eframe::App for MyApp {
 
         egui::CentralPanel::default().show(ctx, |ui| {
             match self.current_lesson {
-                Lessons::Less1 => Self::draw_lesson1(ctx, ui),
+                Lessons::Less1 => self.draw_lesson1(ctx),
                 Lessons::Less2 => Self::draw_lesson2(ctx, ui),
                 Lessons::Less3 => Self::draw_lesson3(ctx, ui),
                 Lessons::Less4 => Self::draw_lesson4(ctx, ui),
